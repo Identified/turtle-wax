a6ab16cdfb0cad85c659a83a81151578d16827a6
Dennis Best <git@dennisbest.org>
2013-01-04 12:22:50 -0800

    Clears out JS file

--- javascripts/custom.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  $("head").append("<link>");
-  var css = $("head").children(":last");
-  css.attr({
-    rel:  "stylesheet",
-    type: "text/css",
-    href: "https://raw.github.com/Identified/turtle-wax/master/stylesheets/main.css"
-  });
-});  
