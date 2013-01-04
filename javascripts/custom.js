b003c09795a7874aaaf6daff6e5c81bab8b3cf17
ramkumarceg <ramkumarceg@gmail.com>
2013-01-04 15:45:14 -0800

    remove nivo slider and add basic slider

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -5,8 +5,30 @@ $('#home-content').load('http://employers.identified.com/Portals/95329/health_re
 
 
 // The BEST way to do tabs
-  $("#tabs div").bind('click', function (e) {
-    this.targetClass = $(e.currentTarget).attr("class");
-    $("#tabs div, #panels div").removeClass("selected");
-    $("." + this.targetClass).addClass('selected');
-  });
+$("#tabs div").bind('click', function (e) {
+this.targetClass = $(e.currentTarget).attr("class");
+$("#tabs div, #panels div").removeClass("selected");
+$("." + this.targetClass).addClass('selected');
+});
+
+// slideshow scripts
+
+jQuery(document).ready(function($) {
+
+    $('#slideshow').bjqs({
+        height      : 320,
+        width       : 960,
+        responsive  : true,
+        // animation values
+        animtype : 'fade', // accepts 'fade' or 'slide'
+        animduration : 450, // how fast the animation are
+        animspeed : 4000, // the delay between each slide
+        automatic : true, // automatic
+
+        // control and marker configuration
+        showcontrols : false, // show next and prev controls
+        showmarkers : true, // Show individual slide markers
+        centermarkers : false // Center markers horizontally
+    });
+
+});
