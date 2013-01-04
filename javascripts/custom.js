bfc84f90bfe7816b77217202b77c9c459f897170
Dennis Best <git@dennisbest.org>
2013-01-04 15:06:51 -0800

    Adds tabs to the features page

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -2,3 +2,11 @@ $('#footer').load('http://employers.identified.com/Portals/95329/health_recruit/
 $('#header').load('http://employers.identified.com/Portals/95329/health_recruit/html/header.html');
 $('#features-content').load('http://employers.identified.com/Portals/95329/health_recruit/html/features/content.html');
 $('#home-content').load('http://employers.identified.com/Portals/95329/health_recruit/html/home/content.html');
+
+
+// The BEST way to do tabs
+$("#tabs div").bind('click', function (e) {
+  this.targetClass = $(e.currentTarget).attr("class");
+  $("#tabs div, #content div").removeClass("selected");
+  $("." + this.targetClass).addClass('selected');
+});
