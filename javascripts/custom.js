62dc5827440684d99e4dc69c6efc32101bfff24c
Dennis Best <git@dennisbest.org>
2013-01-04 12:18:28 -0800

    Clears out custom JS

--- javascripts/custom.js
+++
@@ -1,4 +0,0 @@
-$('#footer').load('http://htmlpreview.github.com/?https://github.com/Identified/turtle-wax/blob/master/html/footer.html');
-$('#header').load('http://employers.identified.com/Portals/95329/health_recruit/header.html');
-$('#home-content').load('http://employers.identified.com/Portals/95329/health_recruit/home/content.html');
-$('#features-content').load('http://employers.identified.com/Portals/95329/health_recruit/features/content.html');
