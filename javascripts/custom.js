d9c6f540dc035ea5385d44cc4b8aaba2d35e9424
Dennis Best <git@dennisbest.org>
2013-01-03 16:14:15 -0800

    Updates custom CSS to load footer

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -1,4 +1,4 @@
-$('#footer').load('https://raw.github.com/Identified/turtle-wax/master/html/footer.html');
+$('#foxboro_footer').load('https://raw.github.com/Identified/turtle-wax/master/html/footer.html');
 $('#header').load('https://raw.github.com/Identified/turtle-wax/master/html/header.html');
 $('#home-content').load('https://raw.github.com/Identified/turtle-wax/master/html/home/content.html');
 $('#features-content').load('https://raw.github.com/Identified/turtle-wax/master/html/features/content.html');
