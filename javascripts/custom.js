a01f0d9f7d5a111e521e1c0aa72b6a69554049c6
Dennis Best <git@dennisbest.org>
2013-01-04 14:32:41 -0800

    Updates Custom.js

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -1,4 +1,4 @@
 $('#footer').load('http://employers.identified.com/Portals/95329/health_recruit/html/footer.html');
-$('#header').load('http://employers.identified.com/Portals/95329/health_recruit/html/footer.html');
+$('#header').load('http://employers.identified.com/Portals/95329/health_recruit/html/header.html');
 $('#features-content').load('http://employers.identified.com/Portals/95329/health_recruit/html/features/content.html');
 $('#home-content').load('http://employers.identified.com/Portals/95329/health_recruit/html/home/content.html');
