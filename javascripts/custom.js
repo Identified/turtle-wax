3788cb9aaef4c333f998859451046e1ed466a5c7
Dennis Best <git@dennisbest.org>
2013-01-07 15:13:48 -0800

    Revises custom.js

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -1,7 +1,17 @@
-$('#footer').load('http://employers.identified.com/Portals/95329/health_recruit/html/footer.html');
-$('#header').load('http://employers.identified.com/Portals/95329/health_recruit/html/header.html');
-$('#features-content').load('http://employers.identified.com/Portals/95329/health_recruit/html/features/content.html');
-$('#home-content').load('http://employers.identified.com/Portals/95329/health_recruit/html/home/content.html');
+// Uncomment this and copy the html to the appropriate pages on HubSpot
+
+        // $('#footer').load('http://employers.identified.com/Portals/95329/health_recruit/html/footer.html');
+        // $('#header').load('http://employers.identified.com/Portals/95329/health_recruit/html/header.html');
+        // $('#features-content').load('http://employers.identified.com/Portals/95329/health_recruit/html/features/content.html');
+        // $('#home-content').load('http://employers.identified.com/Portals/95329/health_recruit/html/home/content.html');
+        
+// Uncomment this to work on stuff via GitHub
+
+
+$('#footer').load('http://employer-cdn.identified.com/html/footer.html');
+$('#header').load('http://employer-cdn.identified.com/html/header.html');
+$('#features-content').load('http://employer-cdn.identified.com/html/features/content.html');
+$('#home-content').load('http://employer-cdn.identified.com/html/home/content.html');
 
 
 // The BEST way to do tabs
