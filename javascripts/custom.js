a75f4cb2e6acd65d8d8fb2a00df34ba35ea16147
Dennis Best <git@dennisbest.org>
2013-01-08 00:19:26 -0800

    Updates scripts

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -5,3 +5,23 @@ jQuery('#tabs div')
 		jQuery_new("." + this.targetClass).addClass('selected');
 		return false; 
 }); 
+
+
+$('#home-content').load(function() {
+	alert('foo');
+	
+	jQuery_new('#slideshow').bjqs({
+			height      : 400,
+			width       : 960,
+			responsive  : true,
+			animtype : 'fade', // accepts 'fade' or 'slide'
+			animduration : 450, // how fast the animation are
+			animspeed : 10000, // the delay be horizontally
+			showcontrols : false, // show next and prev controls
+			centercontrols : false, // center controls verically
+			nexttext : 'Next', // Text for 'next' button (can use HTML)
+			prevtext : 'Prev', // Text for 'previous' button (can use HTML)
+			showmarkers : true, // Show individual slide markers
+			centermarkers : false // Center markers horizontally
+	});	
+});
