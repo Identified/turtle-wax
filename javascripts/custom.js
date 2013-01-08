416b3d8785477af8706d2347c3e650915423e2e4
Dennis Best <git@dennisbest.org>
2013-01-07 22:11:38 -0800

    Updates scripts

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -1,14 +1,7 @@
 $('#tabs div') 
 .livequery('click', function(event) { 
-		alert('clicked'); 
 		this.targetClass = $(event.currentTarget).attr("class");
 		$("#tabs div, #panels div").removeClass("selected");
 		$("." + this.targetClass).addClass('selected');
 		return false; 
 }); 
-
-jQuery("#tabs div").bind('click', function (e) {
-  this.targetClass = $(e.currentTarget).attr("class");
-  $("#tabs div, #panels div").removeClass("selected");
-  $("." + this.targetClass).addClass('selected');
-});
