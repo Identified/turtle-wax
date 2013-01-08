a104fdf0403c7dab5e3437352b9e27685cb2f88b
Dennis Best <git@dennisbest.org>
2013-01-07 16:40:27 -0800

    Updated custom.js

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -8,7 +8,7 @@
 // Uncomment this to work on stuff via GitHub
 
 
-JQuery('#footer').load('http://employer-cdn.identified.com/html/footer.html');
-JQuery('#header').load('http://employer-cdn.identified.com/html/header.html');
-JQuery('#features-content').load('http://employer-cdn.identified.com/html/features/content.html');
-JQuery('#home-content').load('http://employer-cdn.identified.com/html/home/content.html');
+jq142('#footer').load('http://employer-cdn.identified.com/html/footer.html');
+jq142('#header').load('http://employer-cdn.identified.com/html/header.html');
+jq142('#features-content').load('http://employer-cdn.identified.com/html/features/content.html');
+jq142('#home-content').load('http://employer-cdn.identified.com/html/home/content.html');
