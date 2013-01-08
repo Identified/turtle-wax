42288678608e33947bd250ea41338e12ea5c449e
Dennis Best <git@dennisbest.org>
2013-01-07 21:28:00 -0800

    More js experimentation

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -16,18 +16,13 @@
 
 
 function loadContents(url, callback) {  
-
-	//CONFIRM A URL WAS PROVIDED  
 	if(url) {  
-
-		//SET URL FOR YAHOO YQL QUERY
 		var yql = 'http://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from html where url="' + url + '"') + '&format=xml&callback=?';  
 
-		//MAKE YAHOO YQL QUERY  
 		$.getJSON(yql,function(data) {
 
 			//BUILD CALLBACK FUNCTION
-			if(typeof callback === 'function') {  
+			if(typeof callback === 'function') {
 				callback(data.results[0]);  
 			}  
 
@@ -35,29 +30,15 @@ function loadContents(url, callback) {
 		}).error(function(jqXHR, textStatus, errorThrown) { 
 			console.log(errorThrown); }
 		);
-
-  	//LOG ERROR IF NO URL WAS PASSED TO THE SCRIPT
-  	} else {
-  		 console.log('No site was passed to the script.'); 
-  	}
-
+		} else {
+			console.log('No site was passed to the script.');
+		}
 } 
 
-/********************************************************************************
-SAMPLE USAGE
-********************************************************************************/ 
 loadContents('http://employer-cdn.identified.com/html/footer.html', function(results) {  
    $('#footer').html(results); 
 });
 
 loadContents('http://employer-cdn.identified.com/html/header.html', function(results) {  
    $('#header').html(results); 
-});
-
-loadContents('http://employer-cdn.identified.com/html/features/content.html', function(results) {  
-   $('#features-content').html(results); 
-});
-
-loadContents('http://employer-cdn.identified.com/html/home/content.html', function(results) {  
-   $('#home-content').html(results); 
 });
