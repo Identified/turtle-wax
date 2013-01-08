2f4514df098be23f5c41155d3608e89e67ac0479
Dennis Best <git@dennisbest.org>
2013-01-08 01:00:14 -0800

    Updates scripts

--- javascripts/ajax.js
+++ javascripts/ajax.js
@@ -2,7 +2,7 @@ function loadContents(url, callback) {
 	if(url) {  
 		var yql = 'http://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from html where url="' + url + '"') + '&format=xml&callback=?';  
 
-		jQuery_new.getJSON(yql,function(data) {
+		$.getJSON(yql,function(data) {
 
 			//BUILD CALLBACK FUNCTION
 			if(typeof callback === 'function') {
@@ -19,10 +19,10 @@ function loadContents(url, callback) {
 } 
 
 loadContents('http://employer-cdn.identified.com/html/footer.html', function(footer) {  
-   jQuery_new('#footer').html(footer); 
+   $('#footer').html(footer); 
 });
 
 loadContents('http://employer-cdn.identified.com/html/header.html', function(header) {  
-   jQuery_new('#header').html(header); 
+   $('#header').html(header); 
 });
 
