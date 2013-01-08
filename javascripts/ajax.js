e364d9c6fed6d8cb547effe1cb5cad4179664667
Dennis Best <git@dennisbest.org>
2013-01-08 11:19:11 -0800

    Removes remote ajax calls

--- javascripts/ajax.js
+++ javascripts/ajax.js
@@ -1,27 +1,6 @@
 
-
-function loadContents(url, callback) {  
-	if(url) {  
-		var yql = 'http://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from html where url="' + url + '"') + '&format=xml&callback=?';  
-		jQuery_new.getJSON(yql,function(data) {
-			if(typeof callback === 'function') {
-				callback(data.results[0]);  
-			}  
-		}).error(function(jqXHR, textStatus, errorThrown) { 
-			console.log(errorThrown); }
-		);
-		} else {
-			console.log('No site was passed to the script.');
-		}
-} 
-
-// loadContents('http://employer-cdn.identified.com/html/hr-footer.html', function(footer) {  
-//    jQuery_new('#hr-footer').html(footer); 
-// });
-
-loadContents('http://employer-cdn.identified.com/html/hr-header.html', function(header) {  
-   jQuery_new('#hr-header').html(header);
-});
-
+jQuery_new('#hr-header').load('http://employers.identified.com/Portals/95329/health_recruit/html/header.html');
 
 jQuery_new('#hr-footer').load('http://employers.identified.com/Portals/95329/health_recruit/html/footer.html');
+
+jQuery_new('#content-feature').load('http://employers.identified.com/Portals/95329/health_recruit/html/content-feature.html');
