2f6ed357d55b48a713e0dab312ffe49a88c4be9b
Dennis Best <git@dennisbest.org>
2013-01-07 16:02:51 -0800

    Update javascripts/custom.js

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -12,34 +12,3 @@ $('#footer').load('http://employer-cdn.identified.com/html/footer.html');
 $('#header').load('http://employer-cdn.identified.com/html/header.html');
 $('#features-content').load('http://employer-cdn.identified.com/html/features/content.html');
 $('#home-content').load('http://employer-cdn.identified.com/html/home/content.html');
-
-
-
-// slideshow scripts
-
-jQuery(document).ready(function($) {
-
-    $('#slideshow').bjqs({
-        height      : 320,
-        width       : 960,
-        responsive  : true,
-        // animation values
-        animtype : 'fade', // accepts 'fade' or 'slide'
-        animduration : 450, // how fast the animation are
-        animspeed : 15000, // the delay between each slide
-        automatic : true, // automatic
-
-        // control and marker configuration
-        showcontrols : false, // show next and prev controls
-        showmarkers : true, // Show individual slide markers
-        centermarkers : false // Center markers horizontally
-    });
-    
-    // The BEST way to do tabs
-    $("#tabs div").bind('click', function (e) {
-        this.targetClass = $(e.currentTarget).attr("class");
-        $("#tabs div, #panels div").removeClass("selected");
-        $("." + this.targetClass).addClass('selected');
-    });
-
-});
